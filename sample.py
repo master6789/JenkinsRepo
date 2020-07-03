1989e269bfd917976f04179ce07a210da1198e7f
master6789 <44747637+master6789@users.noreply.github.com>
2020-07-03 14:45:35 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
 print "hello world"
 print "this is the updated line"
-print "trying web hook ngrok aliyo sukham..!"
+print "trying web hook ngrok "
+print "SUCCESS FULLY ENABLED GIT HOOK ON LOCAL JENKINS ENIRONMENT WITH ngrok"
