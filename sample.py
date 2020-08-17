446dd964ca593fd6a921432b36eac62e68a98669
master6789 <44747637+master6789@users.noreply.github.com>
2020-08-17 12:53:26 +0530

    Update and rename sample.py to jenkinsFile

--- sample.py
+++
@@ -1,6 +0,0 @@
-print "hello world kooooooooooooooiii"
-print "this is the updated line"
-print "trying web hook ngrok "
-print "SUCCESS FULLY ENABLED GIT HOOK ON LOCAL JENKINS ENIRONMENT WITH ngrok"
-
-
